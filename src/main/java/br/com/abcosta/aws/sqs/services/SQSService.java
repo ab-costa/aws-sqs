a31c5d3068774fa302c14f88cf448c22502799ae
ab-costa <costa.alessandroborges@gmail.com>
2022-05-01 22:39:57 -0300

    feature: Implementação do método sendMessage com sobrecarga na classe SQSService

---
+++ src/main/java/br/com/abcosta/aws/sqs/services/SQSService.java
@@ -0,0 +1,60 @@
+package br.com.abcosta.aws.sqs.services;
+
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+public class SQSService {
+    public static void sendMessage(String message) {
+        AwsCredentialsProvider credentialsProvider = new AwsCredentialsProvider() {
+
+            @Override
+            public AwsCredentials resolveCredentials() {
+                return new AwsCredentials() {
+
+                    @Override
+                    public String accessKeyId() {
+                        return System.getenv("AWS_ACCESS_KEY");
+                    }
+
+                    @Override
+                    public String secretAccessKey() {
+                        return System.getenv("AWS_SECRET_KEY");
+                    }                    
+                };
+            }            
+        };
+        
+        SqsClient sqsClient = SqsClient.builder()
+                                       .region(Region.US_EAST_1)
+                                       .credentialsProvider(credentialsProvider)
+                                       .build();
+
+        GetQueueUrlRequest request = GetQueueUrlRequest.builder()
+                                                       .queueName("fila-teste.fifo")
+                                                       .queueOwnerAWSAccountId("")
+                                                       .build();
+
+        GetQueueUrlResponse queueUrl = sqsClient.getQueueUrl(request);
+
+        sendMessage(sqsClient, queueUrl.queueUrl(), message);
+
+        sqsClient.close();
+    }
+
+    public static void sendMessage(SqsClient sqsClient, String queueUrl, String message) {
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                                                                  .queueUrl(queueUrl)  
+                                                                  .messageGroupId("grupo")
+                                                                  .messageBody(message)
+                                                                  .build();
+        
+        sqsClient.sendMessage(sendMessageRequest);
+    }
+}
